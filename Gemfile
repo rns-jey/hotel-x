405e750cd44e1cea535940aff3a6ce1751f2d4dc
JM Arenas <rns.jey@gmail.com>
2022-04-08 10:28:50 +0800

    Changed default db adapter from pg to sqlite3

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem 'sqlite3', '~> 1.3', '>= 1.3.11'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_08_021215) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
